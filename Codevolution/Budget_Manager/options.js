fd3b0e129d062a6da346987db81af2900011a9d1
anwar.moazam <anwar.palimarwar@gmail.com>
2023-08-14 14:43:39 +0530

    budget manager

---
+++ Codevolution/Budget_Manager/options.js
@@ -0,0 +1,30 @@
+const saveLimitBtn = document.getElementById('saveLimit');
+const resetTotalBtn = document.getElementById('resetTotal');
+
+saveLimitBtn.addEventListener('click', saveLimit);
+resetTotalBtn.addEventListener('click', resetTotal);
+
+chrome.storage.sync.get('limit', function (budget) {
+    document.getElementById('limit').value = budget.limit;
+})
+
+function saveLimit() {
+    let limit = document.getElementById('limit').value;
+    if (limit) {
+        chrome.storage.sync.set({ 'limit': limit }, function () {
+            close();
+        });
+    }
+}
+
+function resetTotal() {
+    chrome.storage.sync.set({ 'total': 0 },function(){
+        let notifOptions = {
+            type: 'basic',
+            iconUrl:'icon48.png',
+            title: 'Total set to Zero',
+            message: "You've set total to Zero"
+        }
+        chrome.notifications.create('limitZero',notifOptions);
+    });
+}
--- Codevolution/Budget_Manager/popup.js
+++ Codevolution/Budget_Manager/popup.js
@@ -1,23 +1,36 @@
 const button = document.getElementById('spendAmount');
 let total = document.getElementById('total');
+let limit = document.getElementById('limit');
 
-chrome.storage.sync.get('total',function(budget){
+
+chrome.storage.sync.get(['total', 'limit'], function (budget) {
     total.innerText = budget.total;
+    limit.innerText = budget.limit;
 })
 
-button.addEventListener('click',updateTotal);
+button.addEventListener('click', updateTotal);
 
-function updateTotal(){
-    chrome.storage.sync.get('total',function(budget){
+function updateTotal() {
+    chrome.storage.sync.get(['total', 'limit'], function (budget) {
         let newTotal = 0;
-        if(budget.total){
+        if (budget.total) {
             newTotal += parseInt(budget.total);
         }
         let amount = document.getElementById('amount').value;
-        if(amount){
+        if (amount) {
             newTotal += parseInt(amount);
         }
-        chrome.storage.sync.set({'total':newTotal});
+        chrome.storage.sync.set({ 'total': newTotal }, function () {
+            if (amount && newTotal >= budget.limit) {
+                let notifOptions = {
+                    type: 'basic',
+                    iconUrl: 'icon48.png',
+                    title: 'Limit reached!',
+                    message: "Uh uh! Looks like you've reached your limit!"
+                };
+                chrome.notifications.create('limitNotif', notifOptions);
+            }
+        });
         total.innerText = newTotal;
         document.getElementById('amount').value = '';
     })
